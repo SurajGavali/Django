d005dbd10c58581dcac71f0e99f457b2f898b51f
SurajGavali <surajgavali1601@gmail.com>
2021-06-13 23:38:38 +0530

    added slug to the end of the url

--- surajgavali/articles/urls.py
+++ surajgavali/articles/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.urls import path,include
 from django.contrib import admin
 from . import views
-#from django.conf.urls import url
+from django.conf.urls import url
 
+app_name = 'articles'
 urlpatterns = [
-    path('',views.article_home, name = 'article_home'),
+    path('',views.article_home, name = 'home'),
+    url(r'^(?P<slug>[\w-]+)/$',views.article_detail,name='detail'),
 ]
