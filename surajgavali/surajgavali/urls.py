25e7c672099ec1e560b3d8f19554f4851a30d29c
SurajGavali <surajgavali1601@gmail.com>
2021-06-14 12:11:46 +0530

    registered accounts urls

--- surajgavali/surajgavali/urls.py
+++ surajgavali/surajgavali/urls.py
@@ -21,9 +21,11 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns #for static 
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
+    path('accounts/',include('accounts.urls')),
     path('about/', views.about),
     path('',views.home),
 ]
