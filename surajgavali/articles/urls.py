06b1a4f71089561fc4f40e507060958759305529
SurajGavali <surajgavali1601@gmail.com>
2021-06-15 12:00:27 +0530

    added url of create article

--- surajgavali/articles/urls.py
+++ surajgavali/articles/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls import url
 app_name = 'articles'
 urlpatterns = [
     path('',views.article_home, name = 'home'),
+    path('create/',views.article_create,name="create"),
     url(r'^(?P<slug>[\w-]+)/$',views.article_detail,name='detail'),
 ]
